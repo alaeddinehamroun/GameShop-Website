f9c35e3414420448b2bb804f1ee0d828594aa7f4
alaeddine <alaeddinehamroun@insat.u-carthage.tn>
2021-08-01 01:15:16 +0100

    frontend work : home ,navbar and footer

---
+++ frontend/src/app/app.routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { SignupComponent } from './examples/signup/signup.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes =[
+    { path: '',             component: HomeComponent },
+    // { path: 'user-profile',     component: ProfileComponent },
+    // { path: 'signup',           component: SignupComponent },
+    // { path: 'landing',          component: LandingComponent },
+    // { path: 'nucleoicons',      component: NucleoiconsComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
